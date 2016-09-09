1226a536a93e600adc4fee3fe3f7c2aeb1518e96
peterainbow <github@bluewhale.plus.com>
2016-09-09 22:14:01 +0100

    change pause

--- motortester.py
+++ motortester.py
@@ -7,42 +7,42 @@ mc.startPwm()
 
 mc.setSpeed(0)
 mc.forwards()
-threading._sleep(.25)
+threading._sleep(1)
 mc.stop()
 
 mc.setSpeed(25)
 mc.forwards()
-threading._sleep(.25)
+threading._sleep(1)
 mc.stop()
 
 mc.setSpeed(75)
 mc.forwards()
-threading._sleep(.25)
+threading._sleep(1)
 mc.stop()
 
 mc.setSpeed(100)
 mc.forwards()
-threading._sleep(.25)
+threading._sleep(1)
 mc.stop()
 
 mc.setSpeed(0)
 mc.backwards()
-threading._sleep(.25)
+threading._sleep(1)
 mc.stop()
 
 mc.setSpeed(25)
 mc.backwards()
-threading._sleep(.25)
+threading._sleep(1)
 mc.stop()
 
 mc.setSpeed(75)
 mc.backwards()
-threading._sleep(.25)
+threading._sleep(1)
 mc.stop()
 
 mc.setSpeed(100)
 mc.backwards()
-threading._sleep(.25)
+threading._sleep(1)
 mc.stop()
 
 
